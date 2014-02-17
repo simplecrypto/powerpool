d9a490bfa3d53fce0fc466050c9dd0e874612960
Isaac Cook <isaac@simpload.com>
2014-02-17 15:37:48 -0600

    Added an example config with comments, tossed a few things from defaults in module

--- powerpool/manager.py
+++ powerpool/manager.py
@@ -17,10 +17,8 @@ def main():
     args = parser.parse_args()
 
     # implement some defaults
-    config = dict(stratum={'port': 8123, 'address': '127.0.0.1'},
+    config = dict(stratum={'port': 3333, 'address': '127.0.0.1'},
                   coinserv=[],
-                  donate_address='',
-                  pool_address='D7QJyeBNuwEqxsyVCLJi3pHs64uPdMDuBa',
                   extranonce_serv_size=8,
                   extranonce_size=4,
                   diff1=0x0000FFFF00000000000000000000000000000000000000000000000000000000)
@@ -30,25 +28,28 @@ def main():
 
     # stored state of all greenlets. holds events that can be triggered, etc
     client_states = {}
-    net_state = {'live_connections': [],
-                 'down_connections': [],
-                 # current known height of blockchain. used to track if we
-                 # need to reset our mining clients
-                 'current_height': 0,
-                 # a collection of known transaction objects
-                 'transactions': {},
-                 # event to broadcast a block being found by workers
-                 'block_found': Event(),
-                 # Store completed block information here for network monitor
-                 # to broadcase
-                 'complete_block': None,
-                 # index of all jobs currently accepting work. Contains complete
-                 # block templates
-                 'jobs': {},
-                 'latest_job': None,
-                 'job_counter': 0,
-                 # the difficulty that will be transmitted to clients
-                 'difficulty': 4}
+    net_state = {
+        # rpc connections in either state
+        'live_connections': [],
+        'down_connections': [],
+        # current known height of blockchain. used to track if we
+        # need to reset our mining clients
+        'current_height': 0,
+        # a collection of known transaction objects
+        'transactions': {},
+        # event to broadcast a block being found by workers
+        'block_found': Event(),
+        # Store completed block information here for network monitor
+        # to broadcase
+        'complete_block': None,
+        # index of all jobs currently accepting work. Contains complete
+        # block templates
+        'jobs': {},
+        'latest_job': None,
+        'job_counter': 0,
+        # the difficulty that will be transmitted to clients
+        'difficulty': 4}
+
     # shared data between greenlets, like the block they should be on, etc
     logger = PrintLogger()
     # start the stratum server reactor thread
