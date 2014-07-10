0cda558eb2b8e6b317fa0317cc6133a9f16c7790
Isaac Cook <isaac@simpload.com>
2014-07-10 18:36:12 -0500

    v0.5.1 official

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='powerpool',
-      version='0.5.0',
+      version='0.5.1',
       description='A pluggable mining pool server implementation',
       author='Isaac Cook',
       author_email='isaac@simpload.com',
