5b3c4df1d970a41c916798eee68c124fbe887f01
Isaac Cook <isaac@simpload.com>
2014-10-13 16:43:15 -0500

    Add reverse proxy X-Script-Name support

--- powerpool/monitor.py
+++ powerpool/monitor.py
@@ -34,6 +34,40 @@ class CustomWSGIHandler(WSGIHandler):
             delta)
 
 
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+
 class ServerMonitor(Component, WSGIServer):
     """ Provides a few useful json endpoints for viewing server health and
     performance. """
@@ -46,6 +80,7 @@ class ServerMonitor(Component, WSGIServer):
     def __init__(self, config):
         self._configure(config)
         app = Flask(__name__)
+        app.wsgi_app = ReverseProxied(app.wsgi_app)
         app.config.update(self.config)
         app.add_url_rule('/', 'general', self.general)
         app.add_url_rule('/debug/', 'debug', self.debug)
